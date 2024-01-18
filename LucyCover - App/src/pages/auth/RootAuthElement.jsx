6d19439207e81ecba0dd092475f60d728d0db97a
jczarnecki014 <czarnecki.web@outlook.com>
2024-01-18 14:00:47 +0100

    works

--- LucyCover - App/src/pages/auth/RootAuthElement.jsx
+++ LucyCover - App/src/pages/auth/RootAuthElement.jsx
@@ -1,12 +1,22 @@
 import { Outlet } from "react-router-dom";
 
+import {Container,Row,Col} from 'react-bootstrap'
+
+
 const RootAuthElement = () => {
     return (
         <>
-            <h1>RootElement</h1>
-            <Outlet />
+            <Container fluid>
+                <Row>
+                   <Col>1</Col>
+                   <Col>2</Col>
+                </Row>
+            </Container>
         </>
     )
 }
 
 export default RootAuthElement;
+
+
+// Dodaj outlet
