13a7fd91fd505bb67b850e3ee052ae7c0ed576b1
jczarnecki014 <czarnecki.web@outlook.com>
2024-01-18 15:24:37 +0100

    fight with img

--- LucyCover - App/src/pages/auth/RootAuthElement.jsx
+++ LucyCover - App/src/pages/auth/RootAuthElement.jsx
@@ -1,15 +1,30 @@
 import { Outlet } from "react-router-dom";
 
-import {Container,Row,Col} from 'react-bootstrap'
+import {Container,Row,Col,Image} from 'react-bootstrap'
+
+import root_page_1 from '../../../public/auth/root_page_1.jpg'
+
+import style from './css/RootAuthElement.module.css'
 
 
 const RootAuthElement = () => {
     return (
         <>
-            <Container fluid>
+            <Container className={style.rootContainer} fluid>
                 <Row>
-                   <Col>1</Col>
-                   <Col>2</Col>
+                   <Col md={6} className={style.backdropColumn}>
+                        <Row className="d-flex">
+                            <Col md={6}>
+                                <Image className='p-0 m-0' src={root_page_1} fluid/>
+                            </Col>
+                            <Col md={6}>
+                                <Image className="p-0 m-0" src={root_page_1} fluid/>
+                            </Col>
+                        </Row>
+                   </Col>
+                   <Col md={6} className={style.formColumn}>
+                        2
+                   </Col>
                 </Row>
             </Container>
         </>
