259b36efd6e2785892592010fef33d3bbe2f3e84
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-05 14:50:58 +0100

    work

--- LucyCover - Frontend/src/components/dashboard/patients/PatientDocumentation/AddPatientDocumentation/AddPatientDocumentation.jsx
+++ LucyCover - Frontend/src/components/dashboard/patients/PatientDocumentation/AddPatientDocumentation/AddPatientDocumentation.jsx
@@ -4,9 +4,16 @@ import AddPatientDocumentation_MENU from "./AddPatientDocumentation_MENU"
 
 const AddPatientDocumentation = () => {
     const [formMode,setFormMode] = useState('menu')
+
+    const FormModeChangeHandler = (mode) => { 
+        setFormMode(mode)
+    }
+
     return (
         <>
-            {formMode === 'menu' && <AddPatientDocumentation_MENU />}
+            {formMode === 'menu' && <AddPatientDocumentation_MENU onModeChange={FormModeChangeHandler} />}
+            {formMode === 'FirstDocumentation' && <AddPatientDocumentation_MENU onModeChange={FormModeChangeHandler} />}
+            {formMode === 'NextDocumentation' && <AddPatientDocumentation_MENU onModeChange={FormModeChangeHandler} />}
         </>
     )
 }
--- LucyCover - Frontend/src/components/dashboard/patients/PatientDocumentation/AddPatientDocumentation/AddPatientDocumentation_MENU.jsx
+++ LucyCover - Frontend/src/components/dashboard/patients/PatientDocumentation/AddPatientDocumentation/AddPatientDocumentation_MENU.jsx
@@ -2,14 +2,14 @@ import OverlayModel from "../../../../utility/OverlayModel"
 
 import style from './css/AddPatientDocumentation_MENU.module.css'
 
-const AddPatientDocumentation_MENU = () => {
+const AddPatientDocumentation_MENU = ({onModeChange}) => {
     return (
         <OverlayModel title="Dodaj nową dokumentacje" smallSize={true}>
             <div className={style.Container}>
                 <h3>Wybierz rodzaj wprowadzanej dokumentacji</h3>
                 <div className={style.ButtonSection}>
-                    <button id={style.FirstDocumentation} >Pierwszorazowa wizyta</button>
-                    <button id={style.NextDocumentation} >Kolejna wizyta (domowa)</button>
+                    <button id={style.FirstDocumentation} onClick={()=>onModeChange('FirstDocumentation')} >Pierwszorazowa wizyta</button>
+                    <button id={style.NextDocumentation} onClick={()=>onModeChange('NextDocumentation')}>Kolejna wizyta (domowa)</button>
                 </div>
             </div>
         </OverlayModel>
